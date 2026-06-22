1a043187966b35014795a56d07e1d09a3c816338
Taynara Jaegger <taynaraj@gmail.com>
2026-06-22 17:04:05 -0300

    feat: estrutura html semantica e componentes base

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 Little Lemon. All rights reserved.</p>
+      <p>Contact: contact@littlelemon.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <img src="" alt="Little Lemon logo" />
+    </header>
+  );
+}
+
+export default Header;
